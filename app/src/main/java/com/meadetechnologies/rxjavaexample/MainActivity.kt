1780aa899f08e58ebf142776152de6b56f2dd8da
Nathan <nathan.r.meade@gmail.com>
2020-05-18 19:27:11 -0400

    feat: Observable created from a single object

--- app/src/main/java/com/meadetechnologies/rxjavaexample/MainActivity.kt
+++ app/src/main/java/com/meadetechnologies/rxjavaexample/MainActivity.kt
@@ -5,9 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.meadetechnologies.rxjavaexample.models.Task
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.BackpressureStrategy
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.core.*
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.functions.Consumer
@@ -19,69 +17,42 @@ class MainActivity : AppCompatActivity() {
 
     val TAG = "RxJavaDebugLog"
 
-    val disposables = CompositeDisposable()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val taskObservable = Observable.fromIterable(createTasksList())
-                .subscribeOn(Schedulers.io())
-                .filter { t ->
-                    Log.d(TAG, "test: ${Thread.currentThread().name}")
-                    try {
-                        Thread.sleep(1000)
-                    } catch (e: InterruptedException){
-                        e.printStackTrace()
+        val task = Task("Walk the dog", false, 3)
+
+        val taskObservable = Observable
+                .create(object : ObservableOnSubscribe<Task>{
+                    override fun subscribe(emitter: ObservableEmitter<Task>?) {
+                        if (!emitter!!.isDisposed){
+                            emitter.onNext(task)
+                            emitter.onComplete()
+                        }
                     }
-                    t?.isComplete!!
-                }
+
+                })
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
 
-
-
-        taskObservable.subscribe(object : Observer<Task> {
+        taskObservable.subscribe(object : Observer<Task>{
             override fun onComplete() {
-                Log.d(TAG, "onComplete called.")
+
             }
 
             override fun onSubscribe(d: Disposable?) {
-                Log.d(TAG, "onSubscribe called")
-                disposables.add(d)
+
             }
 
             override fun onNext(t: Task?) {
-                Log.d(TAG, "onNext: ${Thread.currentThread().name}")
-                if (t != null) {
-                    Log.d(TAG, "onNext: ${t.description}")
-                }
+                Log.d(TAG, "onNext: " + t!!.description)
             }
 
             override fun onError(e: Throwable?) {
-                Log.e(TAG, "onError: ", e)
+
             }
 
         })
-
-        disposables.add(taskObservable.subscribe(object : Consumer<Task>{
-            override fun accept(t: Task?) {
-
-            }
-        }))
-    }
-
-    private fun createTasksList() : List<Task>{
-        val tasks = ArrayList<Task>()
-        tasks.add(Task("Take out the trash", true, 3))
-        tasks.add(Task("Walk the dog", false, 2))
-        tasks.add(Task("Make my bed", true, 1))
-        tasks.add(Task("Unload the dishwasher", false, 0))
-        tasks.add(Task("Make dinner", true, 5))
-        return tasks
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        disposables.clear()
     }
 }
