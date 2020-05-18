41b71f8e634d4c2cc31fe9c28871edaf48d2cc2b
Nathan <nathan.r.meade@gmail.com>
2020-05-18 19:37:11 -0400

    feat: Observable created from a list of objects

--- app/src/main/java/com/meadetechnologies/rxjavaexample/MainActivity.kt
+++ app/src/main/java/com/meadetechnologies/rxjavaexample/MainActivity.kt
@@ -21,13 +21,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val task = Task("Walk the dog", false, 3)
+        /*val task = Task("Walk the dog", false, 3)*/
 
         val taskObservable = Observable
                 .create(object : ObservableOnSubscribe<Task>{
                     override fun subscribe(emitter: ObservableEmitter<Task>?) {
+                        for (task in createTasksList()){
+                            if (!emitter!!.isDisposed){
+                                emitter.onNext(task)
+                            }
+                        }
                         if (!emitter!!.isDisposed){
-                            emitter.onNext(task)
                             emitter.onComplete()
                         }
                     }
@@ -54,5 +58,15 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun createTasksList() : List<Task>{
+        val tasks = ArrayList<Task>()
+        tasks.add(Task("Take out the trash", true, 3))
+        tasks.add(Task("Walk the dog", false, 2))
+        tasks.add(Task("Make my bed", true, 1))
+        tasks.add(Task("Unload the dishwasher", false, 0))
+        tasks.add(Task("Make dinner", true, 5))
+        return tasks
     }
 }
