296e9848e239992c18dadee3d28f824022a40b42
Nathan <nathan.r.meade@gmail.com>
2020-05-19 14:26:56 -0400

    feat: Filter by string implemented

--- app/src/main/java/com/meadetechnologies/rxjavaexample/MainActivity.kt
+++ app/src/main/java/com/meadetechnologies/rxjavaexample/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.meadetechnologies.rxjavaexample.models.Task
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.functions.Predicate
 import io.reactivex.rxjava3.schedulers.Schedulers
 import okhttp3.ResponseBody
 import java.io.IOException
@@ -21,10 +24,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-/*        val taskObservable = Observable
-                .fromIterable(createTasksList())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
+        val taskObservable = Observable
+            .fromIterable(createTasksList())
+            .filter(object : Predicate<Task>{
+                override fun test(t: Task?): Boolean {
+                    return t!!.description.equals("Walk the dog")
+                }
+
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
 
         taskObservable.subscribe(object : Observer<Task>{
             override fun onComplete() {
@@ -36,28 +45,14 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onNext(t: Task?) {
-                Log.d(TAG, "onNext: : " + t!!.description)
+                Log.d(TAG, "onNext: This task matches the description: " + t!!.description)
             }
 
             override fun onError(e: Throwable?) {
 
             }
 
-        })*/
-
-        val mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-            mainViewModel.makeQuery()
-                .observe(this, object : androidx.lifecycle.Observer<ResponseBody>{
-                    override fun onChanged(t: ResponseBody?) {
-                        Log.d(TAG, "onChanged: this is a live data response")
-                        try {
-                            Log.d(TAG, "onChanged: ${t!!.string()}")
-                        } catch (e: IOException) {
-                            e.printStackTrace()
-                        }
-                    }
-
-                })
+        })
     }
 
     private fun createTasksList() : List<Task>{
